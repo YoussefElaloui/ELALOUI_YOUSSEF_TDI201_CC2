46546d632a44094e42df9112209ea23487af022b
YoussefElaloui <youss.2003ala@gmail.com>
2022-06-03 15:11:19 +0100

    Exo 4

--- app/src/main/java/com/example/elaloui_youssef_tdi201_cc2/MyDatabase.java
+++ app/src/main/java/com/example/elaloui_youssef_tdi201_cc2/MyDatabase.java
@@ -7,15 +7,23 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class MyDatabase extends SQLiteOpenHelper {
 
     public static String db_name="societes.db";
+    public static String table_name="Societe";
+    public static String col1="ID";
+    public static String col2="Raison_Sociale";
+    public static String col3="Secteur_activite";
+    public static String col4="nb_employe";
     public MyDatabase(Context context){super(context,db_name,null,1);}
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+        String sql=String.format("create table %s(%s integer autoincrement primary key,%s text,%s text,%s integer)",table_name,col1,col2,col3,col4);
+        sqLiteDatabase.execSQL(sql);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+        String sql="drop table "+table_name;
+        sqLiteDatabase.execSQL(sql);
+        onCreate(sqLiteDatabase);
     }
 }
