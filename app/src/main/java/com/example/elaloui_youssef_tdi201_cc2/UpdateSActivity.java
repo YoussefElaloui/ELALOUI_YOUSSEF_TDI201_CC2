fa858cd8347c7aea20d0ebb6e4cb0c4fec73b3bc
YoussefElaloui <youss.2003ala@gmail.com>
2022-06-03 16:37:30 +0100

    Exo 11-d

--- app/src/main/java/com/example/elaloui_youssef_tdi201_cc2/UpdateSActivity.java
+++ app/src/main/java/com/example/elaloui_youssef_tdi201_cc2/UpdateSActivity.java
@@ -3,6 +3,8 @@ package com.example.elaloui_youssef_tdi201_cc2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,18 +18,41 @@ public class UpdateSActivity extends AppCompatActivity {
     Button update,delete;
     Spinner cmb;
     MyDatabase db;
+    ArrayList<Societe> ss;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_sactivity);
         db=new MyDatabase(this);
         cmb=findViewById(R.id.cmb);
-        ArrayList<Societe> ss=MyDatabase.getAllSociete(db.getReadableDatabase());
+        nom=findViewById(R.id.txt_nomV2);
+        sec=findViewById(R.id.txt_secteur2);
+        nb=findViewById(R.id.txt_nbEmployeV2);
+        update=findViewById(R.id.btn_updateNewS);
+        delete=findViewById(R.id.btn_deleteNewS);
+        ss=MyDatabase.getAllSociete(db.getReadableDatabase());
         ArrayList<String> lst=new ArrayList<>();
         for (Societe s:ss){
             lst.add(String.valueOf(s.getId()));
         }
         ArrayAdapter<String> ad=new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item,lst);
         cmb.setAdapter(ad);
+
+        cmb.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Societe s=ss.get(i);
+                nom.setText(s.getNom());
+                sec.setText(s.getSecteur());
+                nb.setText(s.getNbEmploye());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
     }
 }
