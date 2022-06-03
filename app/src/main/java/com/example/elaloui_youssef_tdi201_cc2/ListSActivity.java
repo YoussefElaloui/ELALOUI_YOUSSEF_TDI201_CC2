b9ed97a0ca20992fea9e2f37d6437a33747b3f0e
YoussefElaloui <youss.2003ala@gmail.com>
2022-06-03 15:53:58 +0100

    Exo 7

---
+++ app/src/main/java/com/example/elaloui_youssef_tdi201_cc2/ListSActivity.java
@@ -0,0 +1,14 @@
+package com.example.elaloui_youssef_tdi201_cc2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListSActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_sactivity);
+    }
+}
--- app/src/main/java/com/example/elaloui_youssef_tdi201_cc2/MainActivity.java
+++ app/src/main/java/com/example/elaloui_youssef_tdi201_cc2/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.elaloui_youssef_tdi201_cc2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void derecTo(View view) {
+        Intent i=null;
+        switch(view.getId()){
+            case R.id.btn_addS: i=new Intent(MainActivity.this,AddSActivity.class);break;
+            case R.id.btn_updateS: i=new Intent(MainActivity.this,UpdateSActivity.class);break;
+            case R.id.btn_listS: i=new Intent(MainActivity.this,ListSActivity.class);break;
+        }
+        startActivity(i);
     }
 }
